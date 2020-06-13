62a5a7dfee626e2082bc53466ceca555eef496c3
Moctezuma Rodriguez <mocrodri@cisco.com>
2020-06-13 17:54:34 -0500

    Adding back PIM... into gitignore

--- PIM-SM_BSR.py
+++ PIM-SM_BSR.py
@@ -6,5 +6,5 @@ Goals of BSR :
     Get a standard to distribute RP mapping information
     Do NOT use multicast to distribute information. Instead, informations are propagated hop by hop, like Spanning Tree BPDU. This tackles one of the big drawback of Auto-RP which is the need to use dense-mode or static RP for the 2 Auto-RP groups.
     Provide more flexibility in RP election. With Auto-RP, if there are 2 candidates RPs for the same set of groups, only one is used (highest IP). BSR allows to use both of them by selecting RP based on a hash.
-
+aaaa
 """
